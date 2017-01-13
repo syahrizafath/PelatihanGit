36470c97f5be7124009010f9e77e95e67b37ac4c
syahrizafath <muhammad.9255@students.amikom.ac.id>
2017-01-13 22:18:03 +0700

    Make new variabel in index.php

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ require __DIR__.'/../bootstrap/autoload.php';
 | the responses back to the browser and delight our users.
 |
 */
-
+$test = "whatever";
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
